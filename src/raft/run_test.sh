40017d99f574f38446dccf8464734df1ee5a45ef
cbb1996 <cbb1996@sp23-cs425-2805.cs.illinois.edu>
2023-03-31 15:34:09 -0500

    Adding script to run tests.

---
+++ src/raft/run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+testName=$1
+numIterations=$2
+testsPerIteration=$3
+for (( i=1; i<=$numIterations; i++ ))
+do
+	for (( j=1; j<=$testsPerIteration; j++ ))
+        do
+                go test -v -run ${testName} > ${testName}_${i}_${j}.log;
+                res=$(cat ${testName}_${i}_${j}.log | grep "Passed")
+		if [ ! -z "${res}" ]
+                then
+                        echo "${testName}_${i}_${j}.log passed!"
+                        rm -rf ${testName}_${i}_${j}.log
+                else
+                echo "${testName}_${i}_${j}.log failed! Retaining log."
+fi
+        done;
+done
